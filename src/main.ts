e0626b63a8295d25b6576d1f453b83f19d945b11
Jeiel Alves <jeiel2013@users.noreply.github.com>
2026-07-28 12:34:10 -0300

    feat: serve arquivos estáticos da pasta de uploads

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { Logger } from 'nestjs-pino';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,7 +9,11 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), { prefix: '/uploads' });
 
   app.useLogger(app.get(Logger));
   app.use(helmet());
