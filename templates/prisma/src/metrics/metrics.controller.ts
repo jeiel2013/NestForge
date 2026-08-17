922a0510dbe9c8dbbe57222678831920387d2937
Jeiel Alves <jeiel2013@users.noreply.github.com>
2026-08-17 12:35:20 -0300

    feat: marca o decorator de Swagger no MetricsController como opcional

--- templates/prisma/src/metrics/metrics.controller.ts
+++ templates/prisma/src/metrics/metrics.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+// nestforge:feature:swagger
 import { ApiExcludeController } from '@nestjs/swagger';
+// nestforge:feature:swagger:end
 import { Public } from '../common/decorators/public.decorator';
 import { MetricsService } from './metrics.service';
 
+// nestforge:feature:swagger
 @ApiExcludeController()
+// nestforge:feature:swagger:end
 @Controller('metrics')
 export class MetricsController {
     constructor(private readonly metricsService: MetricsService) { }
