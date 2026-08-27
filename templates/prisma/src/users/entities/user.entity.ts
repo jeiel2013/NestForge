85ea86a4f4e1a39f15b53f8fa325be9115236a51
Jeiel Alves <jeiel2013@users.noreply.github.com>
2026-08-27 12:47:38 -0300

    fix: Corrigir inicialização da entidade de usuário

--- templates/prisma/src/users/entities/user.entity.ts
+++ templates/prisma/src/users/entities/user.entity.ts
@@ -2,14 +2,14 @@ import { Exclude } from 'class-transformer';
 import { Role } from '@prisma/client';
 
 export class UserEntity {
-    id: string;
-    name: string;
-    email: string;
-    role: Role;
-    avatarUrl: string | null;
-    emailVerifiedAt: Date | null;
-    createdAt: Date;
-    updatedAt: Date;
+    id!: string;
+    name!: string;
+    email!: string;
+    role!: Role;
+    avatarUrl!: string | null;
+    emailVerifiedAt!: Date | null;
+    createdAt!: Date;
+    updatedAt!: Date;
 
     @Exclude()
     passwordHash?: string | null;
