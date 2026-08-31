5fd0c8312996be8dc06cf4ba5e1331280ce3b8ae
Jeiel Alves <jeiel2013@users.noreply.github.com>
2026-08-31 20:26:21 -0300

    feat: Adicionar schema PostgreSQL ao template Drizzle

---
+++ templates/drizzle/src/database/schema/index.ts
@@ -0,0 +1,11 @@
+// nestforge:feature:database:postgres
+export * from './postgres.schema';
+// nestforge:feature:database:postgres:end
+
+// nestforge:feature:database:mysql
+export * from './mysql.schema';
+// nestforge:feature:database:mysql:end
+
+// nestforge:feature:database:sqlite
+export * from './sqlite.schema';
+// nestforge:feature:database:sqlite:end
